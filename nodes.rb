4e8bc08db54e3b499add42306a0763029b0fdc43
John Cinnamond <jc@panagile.com>
2014-11-22 23:16:09 +0000

    Basic treetop parser
    Works for tree structure and day range.

---
+++ nodes.rb
@@ -0,0 +1,10 @@
+module TimeRange
+  class DayRange < Treetop::Runtime::SyntaxNode
+  end
+
+  class TimeRange < Treetop::Runtime::SyntaxNode
+  end
+
+  class Repeat < Treetop::Runtime::SyntaxNode
+  end
+end
